a1ca2231885fbbc3f4a106d192053499af8b7051
Dominique Guardiola <dguardiola@quinode.fr>
2012-05-31 15:38:52 +0200

    Django object pattern, in order to match for example a numeric id in URI with the readable object slug for the HTML document I could not avoid a db request, it's ok a not-intensive usage, like a RDF API. Syntax is : - must use named group for the URI pattern , named after the matching field, like (?P<id>\d+)/$ - for the redirect pattern : "http://something.com/@id:my_django_app.my_model_name@/"   the same id field used in the URI pattern, a colon and the app and model names joined with a dot, wrapped into two "@" for easier matching.

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+VERSION = __import__('uriredirect').__version__
+
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'uriredirect',
+    version = VERSION,
+    description = 'URI Redirecttion for Django',
+    packages = ['uriredirect.admin',
+                'uriredirect.http',
+                'uriredirect.models',
+                'uriredirect.tests',
+                'uriredirect.views',
+                ],
+    include_package_data = True,
+    author = 'Ryan Clark',
+    zip_safe = False,
+    install_requires = ['mimeparse', ],
+    long_description = open('README.md').read(),
+    url = 'https://github.com/usgin/uriredirect',
+    download_url = 'https://github.com/usgin/uriredirect/tarball/master',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+
+)
+
